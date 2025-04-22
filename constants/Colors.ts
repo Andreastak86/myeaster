da2ce77c6d2f66e847fbb077aac8369053638b83
Andreastak86 <andreastak.kodehode@gmail.com>
2025-04-22 23:49:33 +0200

    add some content

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,31 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+    light: {
+        text: "#11181C",
+        background: "#FFEB3B",
+        tint: tintColorLight,
+        icon: "#687076",
+        tabIconDefault: "#687076",
+        tabIconSelected: tintColorLight,
+        yellow: "#FFEB3B", // Lys gul
+        orange: "#FFA726", // Myk orange
+        blue: "#81D4FA", // Pastellblå
+        greenYellow: "#CDDC39", // Grønnlig gul
+        brown: "#A1887F", // Mild brun
+    },
+    dark: {
+        text: "#ECEDEE",
+        background: "#151718",
+        tint: tintColorDark,
+        icon: "#9BA1A6",
+        tabIconDefault: "#9BA1A6",
+        tabIconSelected: tintColorDark,
+        yellow: "#FFEB3B",
+        orange: "#FFA726",
+        blue: "#81D4FA",
+        greenYellow: "#CDDC39",
+        brown: "#A1887F",
+    },
 };
